8ee7f69cf4086f99eb6b78e8457ec85d34b110ce
LucaAngioloni <lucaangioloni@gmail.com>
2018-01-18 15:18:47 +0100

    fixed bip sound

--- bip.py
+++ bip.py
@@ -1,3 +1,9 @@
 from playsound import playsound
+import sys, os
 
-playsound("Resources/song.mp3")
+pathname = os.path.dirname(sys.argv[0])
+fullpath = os.path.abspath(pathname).replace(" ", "%20")
+
+print(fullpath)
+
+playsound(fullpath + "/Resources/bip.wav")
