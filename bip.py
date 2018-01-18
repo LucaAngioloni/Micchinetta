ee50a53c710d41c55080b229267416b54bf4055a
LucaAngioloni <lucaangioloni@gmail.com>
2018-01-18 15:24:27 +0100

    fixed merge

--- bip.py
+++ bip.py
@@ -1,15 +1,9 @@
 from playsound import playsound
 import sys, os
 
-<<<<<<< HEAD
 pathname = os.path.dirname(sys.argv[0])
 fullpath = os.path.abspath(pathname).replace(" ", "%20")
 
 print(fullpath)
 
 playsound(fullpath + "/Resources/bip.wav")
-=======
-
-
-playsound("Resources/bip.wav")
->>>>>>> 5af8ef8ff86f5172a5ebe8cf2b279fd40d892261
